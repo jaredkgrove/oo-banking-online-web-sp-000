3c77824b5f5754a24b55d26a96dde4fb64529b35
Jared Grove <jaredkgrove@gmail.com>
2019-04-02 19:33:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
 class Transfer
   attr_reader :sending_account, :receiving_account, :transfer_amount
   def initialize(sending_account, receiving_account, transfer_amount)
-    
+    @sending_account = sending_account
+    @receiving_account = receiving_account
+    @transfer_amount = transfer_amount
   end
 end
