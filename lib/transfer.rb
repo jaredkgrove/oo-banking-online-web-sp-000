3507b7b04c7240a809ccb3962971387c36e13b4f
Jared Grove <jaredkgrove@gmail.com>
2019-04-02 20:11:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,17 +9,13 @@ class Transfer
   end
   
   def valid?
-    puts "#{self.sender.valid?}  sender"
     self.sender.valid? && self.receiver.valid?
   end
   
   def execute_transaction
-    puts self.status
     puts self.valid?
     self.status = "rejected" if !self.valid?
-    puts self.status
     transfer_money if self.status == "pending"
-    puts self.status
     self.status == "complete" ? !self.valid? : "Transaction rejected. Please check your account balance."
   end
   
